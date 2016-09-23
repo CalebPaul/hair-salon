4642e5ae25eb92cef54c2d9b3e0ffde108475e18
Caleb Paul <calebpaul21@gmail.com>
2016-09-23 16:51:42 -0700

    add salon database, noted.md, fix some routing problems, improve ui

---
+++ notes.md
@@ -0,0 +1,8 @@
+ * As a salon employee, I need to be able to see a list of all our stylists.  DONE
+ * As an employee, I need to be able to select a stylist, see their details, and see a list of all clients that belong to that stylist.
+ * As an employee, I need to add new stylists to our system when they are hired.
+ * As an employee, I need to be able to add new clients to a specific stylist.
+ * As an employee, I need to be able to update a stylist's details.
+ * As an employee, I need to be able to update a client's details.
+ * As an employee, I need to be able to delete a stylist if they're no longer employed here.
+ * As an employee, I need to be able to delete a client if they no longer visit our salon.
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,17 +17,27 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/", (request, response) -> {
+    get("/stylists/:id/clients/new", (request, response) -> {
       Map<String, Object> model = new HashMap<>();
-      //add model.put() and logic here
-      //add model.put() and logic here
+      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+      model.put("Stylist", stylist);
+      model.put("template", "templates/stylist-client-form.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/", (request, response) -> {
+    get("/stylists:id/venues", (request, response) -> {
       Map<String, Object> model = new HashMap<>();
-      //add model.put() and logic here
-      //add model.put() and logic here
+      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+      model.put("stylist", stylist);
+      model.put("template", "templates/clients");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/clients", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      // Client client = Client.find(Integer.parseInt(request.params(":id")));
+      model.put("clients", Client.all());
+      model.put("template", "templates/clients.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
@@ -38,31 +48,40 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/", (request, response) -> {
+    get("/stylists", (request, response) -> {
       Map<String, Object> model = new HashMap<>();
-      //add model.put() and logic here
-      //add model.put() and logic here
+      model.put("stylists", Stylist.all());
+      model.put("template", "templates/stylists.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/", (request, response) -> {
+    get("/stylists/:id", (request, response) -> {
       Map<String, Object> model = new HashMap<>();
-      //add model.put() and logic here
-      //add model.put() and logic here
+      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+      model.put("stylist", stylist);
+      model.put("template", "templates/stylist.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    post("/", (request, response) -> {
+    post("/stylists", (request, response) -> {
       Map<String, Object> model = new HashMap<>();
-      //add model.put() and logic here
-      //add model.put() and logic here
+      String name = request.queryParams("name");
+      Stylist newStylist = new Stylist(name);
+      newStylist.save();
+      model.put("stylists", Stylist.all());
+      model.put("template", "templates/stylists.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    post("/", (request, response) -> {
+    post("/clients", (request, response) -> {
       Map<String, Object> model = new HashMap<>();
-      //add model.put() and logic here
-      //add model.put() and logic here
+      Stylist stylist = Stylist.find(Integer.parseInt(request.queryParams(":id")));
+      String name = request.queryParams("name");
+      String cut = request.queryParams("cut");
+      Client newClient = new Client(name, cut, stylist.getId());
+      newClient.save();
+      model.put("stylist", stylist);
+      model.put("template", "templates/client.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
